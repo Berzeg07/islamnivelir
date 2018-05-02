07ca0cd75bf853e2b74f14c13369d7a8f4ff90d3
Aslan <berezg86@gmail.com>
2018-05-02 17:07:29 +0300

    02.05

--- js/common.js
+++ js/common.js
@@ -32,16 +32,7 @@ $(document).ready(function(){
     responsive:{
         0:{
             items:1
-        },
-        480:{
-            items:2 
-        },
-        768:{
-            items:3
-        },  
-          1240:{
-            items:4
-        },     
+        },   
     }
 });
 
